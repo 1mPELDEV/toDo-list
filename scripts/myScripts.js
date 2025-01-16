3fe638c9b19e22a0b75f16ba3317b55061edfeea
PellDEV <pedroasfelipe@gmail.com>
2025-01-16 14:55:34 -0300

    projeto iniciado mas to com porblemas ao add o valor do campo input a concatenação

---
+++ scripts/myScripts.js
@@ -0,0 +1,30 @@
+// MARCAR E DESMARCAR A CAIXINHA
+
+function taskChecked (){
+    const box = document.getElementById("task-checked-box");
+    box.innerHTML = 
+    `<img src="img/caixa-de-selecao.png" alt="" onclick="taskNoChecked()">`
+}
+function taskNoChecked (){
+    const box = document.getElementById("task-checked-box");
+    box.innerHTML = 
+    `<img src="img/square.png" alt="" onclick="taskChecked()">`
+}
+
+// // // CRIAR NOVA TAREFA 
+
+  function novaTarefa() {
+     const inTask = document.getElementById("in-task");
+     const taskBox = document.getElementById("here");  
+    //  const taskValue = inTask.value;
+         taskBox.innerHTML += `
+        <div class="task" id="task-box"> 
+                <div id="task-checked-box">
+                      <img src="img/square.png" alt="" onclick="taskChecked()">
+                </div>
+                 <p id="out-task">${inTask} </p>
+                 <img src="img/delete.png" alt="" onclick="deleteTask()">
+         </div>
+   `;
+   }
+
