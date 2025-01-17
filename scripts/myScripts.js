0e65b23694e05969d8dc1bd36912f4eead150329
PellDEV <pedroasfelipe@gmail.com>
2025-01-17 11:48:54 -0300

    complete

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -16,15 +16,29 @@ function taskNoChecked (){
   function novaTarefa() {
      const inTask = document.getElementById("in-task").value;
      const taskBox = document.getElementById("here");  
+     if( inTask == ""){
+        alert("Insira a tarefa")
+     }
+     else{
      taskBox.innerHTML += `
         <div class="task" id="task-box"> 
                 <div id="task-checked-box">
                       <img src="img/square.png" alt="" onclick="taskChecked()">
                 </div>
                  <p id="out-task">${inTask} </p>
-                 <img src="img/delete.png" alt="" onclick="deleteTask()">
+                 <img src="img/delete.png" alt="" onclick="deleteTask(event)">
          </div>
-   `;
+   `;}
+
+//    taskBox.appendChild(newTask);
+   document.getElementById("in-task").value = "";
    }
 
 
+// DELETAR TAREFA 
+function deleteTask(event) {
+    const task = event.target.parentElement; // Obtém o elemento pai (a div da tarefa)
+    task.remove(); // Remove a tarefa do DOM
+}
+
+//event.target estudar event propiedades como "target"
